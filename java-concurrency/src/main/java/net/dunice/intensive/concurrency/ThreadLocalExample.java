859bde9151aaeb1cdbeb4e1104ea6cb8de40249a
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2025-01-22 11:12:32 +0300

    Fixed thread local example

--- java-concurrency/src/main/java/net/dunice/intensive/concurrency/ThreadLocalExample.java
+++ java-concurrency/src/main/java/net/dunice/intensive/concurrency/ThreadLocalExample.java
@@ -2,8 +2,10 @@ package net.dunice.intensive.concurrency;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,11 +14,15 @@ public final class ThreadLocalExample {
     public static void main(String[] args) {
         try (var executor = Executors.newCachedThreadPool()) {
             final var uniqueCounter = ThreadLocal.withInitial(() -> 0);
+            final var random = new Random();
 
             IntStream.range(0, 100)
                     .mapToObj(i -> executor.submit(() -> {
+                        TimeUnit.SECONDS.sleep(random.nextInt(1, 4));
+
+                        final var before = uniqueCounter.get();
                         uniqueCounter.set(100);
-                        return uniqueCounter.get();
+                        return "Before: %d, after: %d".formatted(before, uniqueCounter.get());
                     })).toList().forEach(future -> {
                         try {
                             System.out.println(future.get());
