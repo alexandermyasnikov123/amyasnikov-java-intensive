7bcba4e5a1ebcd21a654e8ebe59bcf0ba74ca4cf
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2025-02-07 14:48:01 +0300

    Completed code wars simple task

---
+++ brokers-microservices/src/main/java/net/dunice/intensive/brokers_microservices/SpinWords.java
@@ -0,0 +1,40 @@
+package net.dunice.intensive.brokers_microservices;
+
+import lombok.NonNull;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Напишите функцию, которая принимает строку,
+ * состоящую из одного или нескольких слов, и возвращает ту же строку,
+ * но со всеми словами, состоящими из пяти или более букв, в обратном порядке (как в названии этой задачи).
+ * Передаваемые строки будут состоять только из букв и пробелов.
+ * Пробелы будут присутствовать только в том случае, если слов больше одного.
+ * <p>
+ * Примеры:
+ * "Hey fellow warriors"  --> "Hey wollef sroirraw"
+ * "This is a test        --> "This is a test"
+ * "This is another test" --> "This is rehtona test"
+ */
+public class SpinWords {
+    private static final int EXPECTED_LENGTH = 5;
+
+    private static final String WHITESPACE_DELIMITER = " ";
+
+    public static void main(String[] args) {
+        final var examples = List.of("Hey fellow warriors", "This is a test", "This is another test", "test", "stonks");
+        examples.forEach(value -> System.out.printf("Initial string is '%s', modified is '%s'%n", value, spinWords(value)));
+    }
+
+    public static String spinWords(@NonNull String input) {
+        return Arrays.stream(input.split("\\s"))
+                .map(value -> value.length() >= EXPECTED_LENGTH ? reverseString(value) : value)
+                .collect(Collectors.joining(WHITESPACE_DELIMITER));
+    }
+
+    private static String reverseString(@NonNull String input) {
+        final var builder = new StringBuilder(input);
+        return builder.reverse().toString();
+    }
+}
