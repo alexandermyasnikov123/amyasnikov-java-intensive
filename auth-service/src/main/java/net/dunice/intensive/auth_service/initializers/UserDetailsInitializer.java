dfffff2a2cc75aca4c63370c968c25d60b9d187e
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2025-02-10 15:08:26 +0300

    Add properties examples to run this service

--- auth-service/src/main/java/net/dunice/intensive/auth_service/initializers/UserDetailsInitializer.java
+++ auth-service/src/main/java/net/dunice/intensive/auth_service/initializers/UserDetailsInitializer.java
@@ -2,12 +2,15 @@ package net.dunice.intensive.auth_service.initializers;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import net.dunice.intensive.auth_service.configurations.UsersProperties;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserDetailsInitializer {
+    private final UsersProperties properties;
+
     private final UserDetailsManager manager;
 
     @PostConstruct
